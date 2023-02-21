5937909457e77e4a5ce9ff4e980dfac402957432
aishacodes <aishatabdulfatah81@gmail.com>
2023-02-21 15:09:46 +0100

    fix: auth responsiveness

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,16 @@
 import { lazy } from "react";
 import WithSuspense from "../components/WithSuspense";
+import Login from "../pages/Auth/Login";
+import MobileLogin from "../pages/Auth/MobileLogin";
+import Register from "../pages/Auth/Register";
+import MobileRegister from "../pages/Auth/Register/MobileRegister";
 import { IAppRoute } from "../types";
 import {
   HOME,
   LOGIN_PAGE,
   ON_DEMAND_PRINTER,
   SELL_LANDING_PAGE,
+  SIGNUP_PAGE,
 } from "./pathnames";
 
 const HomePage = WithSuspense(lazy(() => import("../pages/Home")));
@@ -19,4 +24,6 @@ export const AppRoutes: IAppRoute[] = [
   { path: HOME, element: <HomePage /> },
   { path: SELL_LANDING_PAGE, element: <SellYourDesignPage /> },
   { path: ON_DEMAND_PRINTER, element: <OnDemandPrinterPage /> },
+  { path: LOGIN_PAGE, element: <MobileLogin /> },
+  { path: SIGNUP_PAGE, element: <MobileRegister /> },
 ];
