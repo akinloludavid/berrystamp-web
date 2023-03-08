c1e84e1f9784fe68e9d52e86edc212feb41e24f4
aishacodes <aishatabdulfatah81@gmail.com>
2023-03-08 14:23:28 +0100

    update

--- src/components/CustomDesignCard/DetailModal.tsx
+++ src/components/CustomDesignCard/DetailModal.tsx
@@ -1,17 +1,21 @@
-import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
 import React, { useRef, useState } from "react";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import { useClickAway } from "react-use";
 import ModalContainer from "../ModalContainer";
 
 const DetailModal = ({ isOpen, onClose }: { isOpen: any; onClose: any }) => {
-  const ref = useRef(null);
-  useClickAway(ref, () => {
-    setshowDropdown(false);
-  });
-  const [showDropdown, setshowDropdown] = useState(false);
-
   return (
     <ModalContainer isOpen={isOpen} onClose={onClose} maxWidth="77rem">
       <Flex
@@ -53,32 +57,20 @@ const DetailModal = ({ isOpen, onClose }: { isOpen: any; onClose: any }) => {
           </Text>
           <Flex gap={"1rem"} position={"relative"}>
             <Button width={"21rem"}>View Order</Button>
-            <Button
-              variant={"outline"}
-              onClick={() => setshowDropdown(!showDropdown)}
-            >
-              <FiMoreHorizontal />
-            </Button>
-            {showDropdown && (
-              <Flex
-                position={"absolute"}
-                right={"0"}
-                top={"2rem"}
-                width={"215px"}
-                bg={"white"}
-                flexDirection={"column"}
-                gap={"18px"}
-                py={"16px"}
-                px={"16px"}
-                borderRadius={"8px"}
-                ref={ref}
-              >
-                <Text variant={"body4"}>Print on Item</Text>
-                <Text variant={"body4"}>Print on mockups</Text>
-                <Text variant={"body4"}>Upload to marketplace</Text>
-                <Text variant={"body4"}>Delete design</Text>
-              </Flex>
-            )}
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<FiMoreHorizontal />}
+                variant="outline"
+              />
+              <MenuList>
+                <MenuItem>Print on Item</MenuItem>
+                <MenuItem>Print on mockups</MenuItem>
+                <MenuItem>Upload to marketplace</MenuItem>
+                <MenuItem>Delete design</MenuItem>
+              </MenuList>
+            </Menu>
           </Flex>
         </Box>
       </Flex>
--- src/components/CustomDesignCard/index.tsx
+++ src/components/CustomDesignCard/index.tsx
@@ -2,25 +2,24 @@ import {
   Box,
   Button,
   Flex,
+  IconButton,
   Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Text,
   useDisclosure,
 } from "@chakra-ui/react";
-import React, { useRef, useState } from "react";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import { useClickAway } from "react-use";
 import { trauncateString } from "../../utils/helper";
 import ModalContainer from "../ModalContainer";
 import DetailModal from "./DetailModal";
 
 const CustomDesignCard = ({ name, img }: { name: string; img: string }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [showDropdown, setshowDropdown] = useState(false);
-  const ref = useRef(null);
-  useClickAway(ref, () => {
-    setshowDropdown(false);
-  });
+
   return (
     <Box
       width={"228px"}
@@ -40,29 +39,20 @@ const CustomDesignCard = ({ name, img }: { name: string; img: string }) => {
         <Text variant={"body4"} fontWeight={"500"}>
           {trauncateString(name, 13)}
         </Text>
-        <span style={{ cursor: "pointer" }}>
-          <FiMoreHorizontal onClick={() => setshowDropdown(!showDropdown)} />
-        </span>
-        {showDropdown && (
-          <Flex
-            position={"absolute"}
-            right={"0"}
-            top={"2rem"}
-            width={"215px"}
-            bg={"white"}
-            flexDirection={"column"}
-            gap={"18px"}
-            py={"16px"}
-            px={"16px"}
-            borderRadius={"8px"}
-            ref={ref}
-          >
-            <Text variant={"body4"}>Print on Item</Text>
-            <Text variant={"body4"}>Print on mockups</Text>
-            <Text variant={"body4"}>Upload to marketplace</Text>
-            <Text variant={"body4"}>Delete design</Text>
-          </Flex>
-        )}
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<FiMoreHorizontal />}
+            variant="outline"
+          />
+          <MenuList>
+            <MenuItem>Print on Item</MenuItem>
+            <MenuItem>Print on mockups</MenuItem>
+            <MenuItem>Upload to marketplace</MenuItem>
+            <MenuItem>Delete design</MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
       <Image
         cursor={"pointer"}
