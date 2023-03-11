2fc7555ae31cc433eab91eb15bbbae872d050702
aishacodes <aishatabdulfatah81@gmail.com>
2023-03-11 20:37:30 +0100

    fix: new collection

--- src/pages/DesignerDashboard/NewCollectionModal.tsx
+++ src/pages/DesignerDashboard/NewCollectionModal.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import React from "react";
 import ModalContainer from "../../components/ModalContainer";
 
@@ -19,7 +27,32 @@ const NewCollectionModal = ({
           Upload and launch new product collections, Inkprint watermark will be
           automatically added to every image upload to protect it. Learn more
         </Text>
-        <Button>Create Collection</Button>
+        <form>
+          <label htmlFor="">Enter collection name*</label>
+          <Input mb={"20px"} mt={"0.5rem"} />
+          <label htmlFor="">Add Description (optional)</label>
+          <Textarea mt={"0.5rem"}></Textarea>
+          <Flex mt={"2rem"} gap={".85rem"}>
+            <Box
+              width={"10rem"}
+              height={"10rem"}
+              borderRadius={"10px"}
+              border={"1px solid #C9C9C9"}
+            ></Box>
+            <Box>
+              Upload Collection Cover{" "}
+              <Text variant={"body3"} color={"#FF0022"} mb={"1rem"}>
+                {" "}
+                (Upload Dimensions: 1200px by
+                <br /> 1200px)
+              </Text>
+              <input type={"file"} />
+            </Box>
+          </Flex>
+          <Button display={"block"} mx={"auto"} mt={"3rem"}>
+            Create Collection
+          </Button>
+        </form>
       </Box>
     </ModalContainer>
   );
